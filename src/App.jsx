91e4e34ce0a2456c939be2dd9e6d0bb458d8964a
alimkin.e@gmail.com <out_alimkin.em@pecom.ru>
2025-02-14 12:33:40 +0300

    using React.elementCreate()

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { createElement } from 'react'
+import { Fragment } from 'react'
 import ReactLogo from './assets/react.svg?react'
 import ViteLogo from './assets/vite.svg?react'
 import './App.css'
@@ -7,30 +9,35 @@ function App() {
 	//Декларативный стиль
 	const [count, setCount] = useState(0)
 	//Декларативный стиль
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<ViteLogo className="logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<ReactLogo className="logo react" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				{/* Императивный стиль, явно указывается что делаем что увеличить count*/}
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-			<p>{new Date().getFullYear()}</p>
-		</>
-	)
+	return createElement(Fragment, null, [
+		createElement('div', null, [
+			createElement(
+				'a',
+				{ href: 'https://vite.dev', target: '_blank' },
+				createElement(ViteLogo, { className: 'logo' }),
+			),
+			createElement(
+				'a',
+				{ href: 'https://react.dev', target: '_blank' },
+				createElement(ReactLogo, { className: 'logo react' }),
+			),
+		]),
+		createElement('h1', null, 'Vite + React'),
+		createElement('div', { className: 'card' }, [
+			createElement(
+				'button',
+				{ onClick: () => setCount((count) => count + 1) },
+				`count is ${count}`,
+			),
+			createElement('p', null, 'Edit <code>src/App.jsx</code> and save to test HMR'),
+		]),
+		createElement(
+			'p',
+			{ className: 'read-the-docs' },
+			'Click on the Vite and React logos to learn more',
+		),
+		createElement('p', null, new Date().getFullYear()),
+	])
 }
 
 export default App
